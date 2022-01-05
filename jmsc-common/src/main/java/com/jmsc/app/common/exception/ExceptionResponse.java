05378efb0431f789d7b39be6ba82bcceba2653a9
csongithub <cs3088@gmail.com>
2022-01-05 19:46:01 +0530

    successfully converted to gradle project

--- jmsc-common/src/main/java/com/jmsc/app/common/exception/ExceptionResponse.java
+++ jmsc-common/src/main/java/com/jmsc/app/common/exception/ExceptionResponse.java
@@ -14,10 +14,19 @@ import lombok.NoArgsConstructor;
  *
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ExceptionResponse {
 	private String message;
 	private String detail;
 	private Date timestamp;
+	
+	
+	public ExceptionResponse() {}
+
+
+	public ExceptionResponse(String message, String detail, Date timestamp) {
+		super();
+		this.message = message;
+		this.detail = detail;
+		this.timestamp = timestamp;
+	}
 }
