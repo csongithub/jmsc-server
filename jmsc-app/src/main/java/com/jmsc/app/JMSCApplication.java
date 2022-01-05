93e1d426ad032506b5fe4732d5c91f68a3ed07be
csongithub <cs3088@gmail.com>
2022-01-05 21:13:07 +0530

    fising gradle

--- jmsc-app/src/main/java/com/jmsc/app/JMSCApplication.java
+++ jmsc-app/src/main/java/com/jmsc/app/JMSCApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 
