1ec3078a4ea54cca224ead393b1e64254ffcd6e7
csongithub <cs3088@gmail.com>
2022-02-16 19:28:56 +0530

    added client & jwt token based authorization

--- jmsc-rest/src/main/java/com/jmsc/app/rest/ClientEndPoint.java
+++ jmsc-rest/src/main/java/com/jmsc/app/rest/ClientEndPoint.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amazonaws.services.waf.model.HTTPRequest;
 import com.jmsc.app.common.dto.ClientDTO;
 import com.jmsc.app.service.ClientService;
 
